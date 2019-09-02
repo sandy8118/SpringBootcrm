8f12bffe61f30cb642eb9781dcd540bf4fca098a
Sandip Adhav <sandip.office3@gmail.com>
2019-09-02 15:51:44 +0530

    Spring Jpa Implementation

--- src/main/java/sandy/springBoot/crm/SpringBootCrm/customerrest/CustomerRest.java
+++ src/main/java/sandy/springBoot/crm/SpringBootCrm/customerrest/CustomerRest.java
@@ -1,9 +1,13 @@
 package sandy.springBoot.crm.SpringBootCrm.customerrest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,9 +31,21 @@ public String demo(){
 @GetMapping("/AllCustomers")
 public List<Customer> getCustomers(){
 	
-	return customerService.getCustomers();
+	return customerService.findAll();
 	
 }	
 	
+@GetMapping("/Customer/{id}")
+public Optional<Customer> getCustomers(@PathVariable int id){
 	
+	return customerService.findById(id);
+	
+}	
+
+@PostMapping("/SaveCustomer")
+public Customer saveCustomer(@RequestBody Customer cust) {
+	
+	return customerService.save(cust);
+}
+
 }
